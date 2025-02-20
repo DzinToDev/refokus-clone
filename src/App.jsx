b328462e93cb99177813c2a00cc10fbfbe82e32b
DzinToDev <maheshdgaikwad18@gmail.com>
2025-02-20 17:26:31 +0530

    project setup completed and working on navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+
+
+function App() {
+  
+
+  return (
+   <div className="bg-zinc-900  w-full h-screen text-white ">
+    <Navbar />
+   </div>
+  )
+}
+
+export default App
