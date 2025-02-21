d81224474a2494163ccff04880b1fdc2a44df242
DzinToDev <maheshdgaikwad18@gmail.com>
2025-02-21 13:26:31 +0530

    remove log

--- src/components/Stripe.jsx
+++ src/components/Stripe.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Stripe = ({val}) => {
-    console.log(val);
+
    
   return (
     <div className='w-1/6 px-4 py-3 border-[0.01px] border-zinc-200 bg-zinc-300 border-r-1 border-r-zinc-700 text-black flex items-center justify-between'>
