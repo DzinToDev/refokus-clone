de8bb0be2b49ac42ff1542c703d44b2616e62836
DzinToDev <maheshdgaikwad18@gmail.com>
2025-02-21 13:20:08 +0530

    border color fix

--- src/components/Stripe.jsx
+++ src/components/Stripe.jsx
@@ -4,7 +4,7 @@ const Stripe = ({val}) => {
     console.log(val);
    
   return (
-    <div className='w-1/6 px-4 py-3 border-[0.01px] border-zinc-200 bg-zinc-300 text-black flex items-center justify-between'>
+    <div className='w-1/6 px-4 py-3 border-[0.01px] border-zinc-200 bg-zinc-300 border-r-1 border-r-zinc-700 text-black flex items-center justify-between'>
         <img className='w-40' src={val.url} style={{color:"white"}} alt="" />
         <span>{val.num}</span>
     </div>
